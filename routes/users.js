c141431107a22eafacd9dc92112b0a4f2988ff67
EDUAR AUGUSTO OJEDA JIMENEZ <eduar.ojeda2220@correo.policia.gov.co>
2018-07-04 20:54:42 -0500

    router  con error

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var object = require('../modules/objectsAndTypes');//importar librerias de CRUD
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({message:'respond with a resource'});
+router.get('/:id', function (req, res, next) {//:id (url contiene un parametro)
+
+  /*object.get('User', req.params.id, 1, null)
+    .then(response => {
+      res.json({
+        status: true,
+        content: response
+        
+      });
+    })
+    .catch(response => {
+      res.json({
+        status: false,
+        content: response
+      });
+    });*/
+
+
 });
 
 module.exports = router;
