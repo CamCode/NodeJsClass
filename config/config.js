74f00b22bd24c67022ea8d38a7eaa73be75f8d09
EDUAR AUGUSTO OJEDA JIMENEZ <eduar.ojeda2220@correo.policia.gov.co>
2018-07-04 20:16:47 -0500

    primer

---
+++ config/config.js
@@ -0,0 +1,23 @@
+let config = {};
+
+config.database = {};
+config.database.dialect = "mysql";
+config.database.username = "root";
+config.database.password = "";
+config.database.port = "3306";
+config.database.host = "127.0.0.1";
+config.database.database = "BD1";
+config.database.operatorsAliases = true;
+
+config.database.pool = {};
+config.database.pool.max = 5;
+config.database.pool.min = 0;
+config.database.pool.acquire = 30000;
+config.database.pool.idle = 10000;
+
+config.sequelize = {};
+config.sequelize.sync = {};
+config.sequelize.sync.force = true;
+//config.sequelize.sync.logging: null;
+
+module.exports = config;
---
+++ models/cart.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+    var Cart = sequelize.define('Cart', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        user: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        nombre: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+            // don't add the timestamp attributes (updatedAt, createdAt)
+            timestamps: false,
+
+            // don't use camelcase for automatically added attributes but underscore style
+            // so updatedAt will be updated_at
+            underscored: true,
+
+            // disable the modification of table names; By default, sequelize will automatically
+            // transform all passed model names (first parameter of define) into plural.
+            // if you don't want that, set the following
+            freezeTableName: true,
+
+            // define the table's name
+            tableName: 'cart',
+
+            // Enable optimistic locking.  When enabled, sequelize will add a version count attribute
+            // to the model and throw an OptimisticLockingError error when stale instances are saved.
+            // Set to true or a string with the attribute name you want to use to enable.
+            version: true
+        });
+
+    Cart.associate = models => {
+        Cart.belongsToMany(models.Product, {
+            as: 'Products',
+            through: 'Carts_Products',
+            foreignKey :'cart'
+        });
+    };
+    return Cart;
+};
