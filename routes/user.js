b4f8cf73e1d65ad672247546760f0595079d1118
EDUAR AUGUSTO OJEDA JIMENEZ <eduar.ojeda2220@correo.policia.gov.co>
2018-07-06 18:44:15 -0500

    METODOS GET POST Y PUT router user

--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,59 @@
 var express = require('express');
 var router = express.Router();
-var object = require('../modules/objectsAndTypes');//importar librerias de CRUD
+var object = require('../modules/objectsAndTypes');
 
 /* GET users listing. */
-router.get('/:id', function (req, res, next) {//:id (url contiene un parametro)
-
+router.get('/:id', (req, res, next) => {
   object.get('User', req.params.id, 1, null)
     .then(response => {
-      res.json({
-        status: true,
-        content: response
-        
-      });
+      res.json({ status: true, content: response });
     })
     .catch(response => {
-      res.json({
-        status: false,
-        content: response
-      });
+      res.json({ status: false, content: response });
     });
+});
 
+router.post('/save', (req, res, next) => {
+  object.save([
+    'nombres',
+    'apellidos',
+    'email',
+    'password',
+    { birthday: 'Date' }
+  ], req.body, 'User')
+    .then(response => {
+      res.json({ status: true, content: response });
+    })
+    .catch(response => {
+      res.json({ status: false, content: response });
+    });
+});
 
+router.put('/save/:id', (req, res, next) => {
+  req.body.id = req.params.id;
+  object.update([
+    'nombres',
+    'apellidos',
+    'email',
+    'password',
+    { birthday: 'Date' }
+  ], req.body, 'User')
+    .then(response => {
+      res.json({ status: true, content: response });
+    })
+    .catch(response => {
+      res.json({ status: false, content: response });
+    });
+});
+
+router.delete('/delete/:id', (req, res, next) => {
+  object.delete('User', req.params.id)
+    .then(response => {
+      res.json({ status: true, content: response });
+    })
+    .catch(response => {
+      res.json({ status: false, content: response });
+    });
 });
 
 module.exports = router;
